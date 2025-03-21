2170e4a60c6227665e6ad1b0065af867898d36fc
Missionkiller2 <m244541maksim2@gmail.com>
2025-03-21 17:46:59 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg update
+pkg upgrate
+pkg install python
+pkg install git
+git clone https://github.com/Missionkiller2/UserbotTg.git
+cd UserbotTg
+unzip -o userbot 
+cd userbot
+clear
+python main.py
+clear
+python bot.py
